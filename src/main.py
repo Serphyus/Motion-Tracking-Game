a9b594ebb7a763f8ee91acb4f2909c6f876fc6f4
Sigurd-Pettersen <73425444+Sigurd-Pettersen@users.noreply.github.com>
2022-03-05 21:25:54 +0100

    removed __name__ checking for executing after being compiled

--- src/main.py
+++ src/main.py
@@ -2,6 +2,5 @@ from pathlib import Path
 from game import Game
 
 
-if __name__ == '__main__':
-    game = Game(Path(__file__).resolve().parent)
-    game.main_loop()
+game = Game(Path(__file__).resolve().parent)
+game.main_loop()
