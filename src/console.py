899601e3aa55caf7361e6839e661688b4e73d769
Sigurd-Pettersen <73425444+Sigurd-Pettersen@users.noreply.github.com>
2022-04-12 16:30:29 +0200

    created console for logging

---
+++ src/console.py
@@ -0,0 +1,28 @@
+import logging
+from rich.logging import RichHandler
+
+
+logging.basicConfig(
+    level='NOTSET',
+    format='%(message)s',
+    datefmt='[%H:%M:%S.%f]',
+    handlers=[RichHandler()]
+)
+
+
+class Logging:
+    _logger = logging.getLogger('rich')
+
+    @classmethod
+    def debug_msg(cls, msg: str) -> None:
+        cls._logger.debug('%s' % msg)
+    
+
+    @classmethod
+    def warning_msg(cls, msg: str) -> None:
+        cls._logger.warning('%s' % msg)
+
+
+    @classmethod
+    def error_msg(cls, msg: str) -> None:
+        cls._logger.error('%s' % msg)
